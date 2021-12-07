fdc924e13430aa397f9c829146b44e0d0baf903d
Miclell <hem123v@gmail.com>
2021-12-07 17:51:07 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace MachineTuring
+{
+    public class Ribbon
+    {
+        public int size = 50;
+        public int startPoint = 25;
+        
+        public string[] value;
+
+        private readonly string alphabet;
+        private readonly List<Dictionary<string, string>> commands = new();
+
+        public Ribbon(string alphabet, string iniValue, int size, int startPoint)
+        {
+            this.alphabet = alphabet;
+            value = iniValue.Select(x => x.ToString()).ToArray();
+            this.size = size;
+            this.startPoint = startPoint;
+        }
+
+        private void AddCommand(string command) =>
+            commands.Add(command.Split(';').ToDictionary(x => x[0].ToString(), x => x[1..]));
+
+        public void AddCommands()
+        {
+            Console.WriteLine("Закончите ввод введя 0\nКоманды вводятся в формате {если этот символ}{на что заменить}{куда дальше(<>.)}{в какую команду} - 01>1;11>2");
+
+            while (true)
+            {
+                string temp = Console.ReadLine();
+
+                if (temp == "0")
+                    break;
+                else
+                    AddCommand(temp);
+            }
+        }
+
+        public void Action()
+        {
+            int i = 0;
+            int nowCommand = 1;
+
+            Console.WriteLine();
+
+            while(true)
+            {
+                string temp = commands[nowCommand - 1][value[startPoint + i]];
+
+                string go = Regex.Matches(temp, @"[<>.]\w").Select(x => x.Value).Aggregate((x, r) => x + r);
+                string rplcmt = Regex.Matches(temp, @"\w[<>.]").Select(x => x.Value).Aggregate((x, r) => x + r)[0].ToString();
+
+                value[startPoint + i] = rplcmt;
+
+                Console.WriteLine(String.Join(null, Enumerable.Repeat(" ", startPoint + i)) + "v");
+
+                Console.WriteLine(String.Join(null, value));
+
+                switch (go[0])
+                {
+                    case '>':
+                        i++;
+                        break;
+                    case '<':
+                        i--;
+                        break;
+                }
+
+                nowCommand = int.Parse(go[1..]);
+
+                if (nowCommand == 0)
+                    break;
+            }
+
+            Console.WriteLine("\nРезультат - " +String.Join(null, value));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ribbon r = new("01", "000111000", 9, 0);
+
+            //01>1;11>2
+            //00>0;11>2
+
+            r.AddCommands();
+
+            r.Action();
+        }
+    }
+}
